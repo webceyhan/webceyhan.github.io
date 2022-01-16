c484759769227c6eca463f9a439450e32ec34809
Evren Ceyhan <evren@ceyhan.io>
2022-01-17 00:51:19 +0300

    repos filtered

--- app.js
+++ app.js
@@ -9,8 +9,10 @@ const GITHUB_URL = 'https://api.github.com/users/webceyhan';
 
 async function getRepositories() {
     const url = DEBUG ? '/data/repos.json' : `${GITHUB_URL}/repos`;
+    const data = await (await fetch(url)).json();
 
-    return await (await fetch(url)).json();
+    // filter repos without homepage (no deployed webapp)
+    return data.filter((repo) => repo.homepage !== '' && repo.homepage != null);
 }
 
 const timestamp = (date) => new Date(date).toLocaleDateString();

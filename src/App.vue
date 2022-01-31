b9e5bcd4f333a3c5f5fe8d63c0525f2cc2278136
Evren Ceyhan <evren@ceyhan.io>
2022-01-31 23:19:47 +0300

    profile comp. impl.

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,11 @@
 
 import { ref, onMounted } from 'vue';
 import { createGithubAPI } from './api/github';
-import Avatar from './components/Avatar.vue'
 import RepositoryList from './components/RepositoryList.vue'
 import SocialLinkNav from './components/SocialLinkNav.vue'
+import Profile from './components/Profile.vue';
 
 const api = createGithubAPI();
-
 const profile = ref({});
 const repos = ref([]);
 const socialLinks = ref([]);
@@ -25,29 +24,7 @@ onMounted(async () => {
     <!-- Header -->
     <header class="py-5 px-md-5">
         <div class="container text-center text-sm-start text-light p-md-5">
-            <div class="row align-items-center">
-                <div class="col-md-4 d-flex justify-content-center">
-                    <avatar :url="profile.avatar_url" />
-                </div>
-
-                <div class="col-md-8">
-                    <h1 class="display-5 text-light text-opacity-50">Hi, I am</h1>
-
-                    <h1 class="display-1 mb-3">{{ profile.name }}</h1>
-
-                    <h4 class="text-light text-opacity-50">
-                        I'm a {{ profile.location }} based
-                        <span
-                            class="text-nowrap text-warning"
-                        >Full Stack Web Developer</span>
-                        at
-                        <img
-                            src="./assets/logo.png"
-                            class="img-fluid w-25"
-                        />
-                    </h4>
-                </div>
-            </div>
+            <profile :profile="profile" />
         </div>
     </header>
 
---
+++ src/components/Profile.vue
@@ -0,0 +1,32 @@
+<script setup>
+
+import Avatar from './Avatar.vue'
+
+defineProps({
+    profile: Object
+})
+</script>
+
+<template>
+    <div class="row align-items-center">
+        <div class="col-md-4 d-flex justify-content-center">
+            <avatar :url="profile.avatar_url" />
+        </div>
+
+        <div class="col-md-8">
+            <!-- intro -->
+            <h1 class="display-5 text-light text-opacity-50">Hi, I am</h1>
+            <h1 class="display-1 mb-3">{{ profile.name }}</h1>
+
+            <!-- description -->
+            <h4 class="text-light text-opacity-50">
+                I'm a {{ profile.location }} based
+                <span
+                    class="text-nowrap text-warning"
+                >Full Stack Web Developer</span>
+                at
+                <img src="../assets/logo.png" class="img-fluid w-25" />
+            </h4>
+        </div>
+    </div>
+</template>

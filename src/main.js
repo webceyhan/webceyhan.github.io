9520e0a925d752baca56e15cb729a9b0984773e3
Evren Ceyhan <evren@ceyhan.io>
2022-01-31 20:50:26 +0300

    bootstrap-icons added as npm package

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import './style.css';
 
 createApp(App).mount('#app');

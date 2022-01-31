bcda9ee0bb4c84d35b1b72a5699f9ca6890ec0a7
Evren Ceyhan <evren@ceyhan.io>
2022-01-31 23:59:12 +0300

    filter system impl.

--- src/components/RepositoryList.vue
+++ src/components/RepositoryList.vue
@@ -1,18 +1,52 @@
 <script setup>
 
+import { computed, ref } from 'vue';
 import Repository from './Repository.vue'
 
-defineProps({
+const props = defineProps({
     repos: Array
 })
+
+const selectedFilter = ref(null);
+
+const filters = computed(() => {
+    return props.repos.reduce((all, { topics }) => new Set([...all, ...topics]), [])
+})
+
+const filteredRepos = computed(() => {
+    return !selectedFilter.value
+        ? props.repos
+        : props.repos.filter(({ topics }) => topics.some(topic => topic === selectedFilter.value))
+})
+
+function onToggleFilter(filter) {
+    if (selectedFilter.value === filter) {
+        selectedFilter.value = null;
+    }
+    else {
+        selectedFilter.value = filter;
+    }
+}
 
 </script>
 
 <template>
     <h1 class="display-4">Projects</h1>
+
+    <nav class="nav">
+        <a
+            v-for="(filter, i) in filters"
+            :key="i"
+            href="#"
+            @click.prevent="onToggleFilter(filter)"
+            class="nav-link link-primary badge rounded-pill bg-dark me-1 mb-1"
+            :class="{ 'text-light': filter === selectedFilter }"
+        >{{ filter }}</a>
+    </nav>
+
     <hr class="bg-white mb-4" />
 
     <ul class="list-group list-group-flush">
-        <repository v-for="repo in repos" :key="repo.id" :repo="repo" />
+        <repository v-for="repo in filteredRepos" :key="repo.id" :repo="repo" />
     </ul>
 </template>

98f75833a20e54c02014c28835a944aaecdf9229
Evren Ceyhan <evren@ceyhan.io>
2023-08-12 12:23:49 +0300

    bootstrap ui removed

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 
-import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import './style.css';
 

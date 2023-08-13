ea60ca8373fc3e017463da12e4021ee152df99fd
Evren Ceyhan <evren@ceyhan.io>
2023-08-14 00:19:25 +0300

    repository hover effects added

--- src/sections/partials/Repository.vue
+++ src/sections/partials/Repository.vue
@@ -6,8 +6,8 @@ import Link from "@/components/Link.vue";
 import Badge from "@/components/Badge.vue";
 import Divider from "@/components/Divider.vue";
 import ProgressBar from "@/components/ProgressBar.vue";
-import TopicList from "./TopicList.vue";
 import LanguageList from "./LanguageList.vue";
+import TopicList from "./TopicList.vue";
 
 defineProps({
   repo: Object,
@@ -15,13 +15,19 @@ defineProps({
 </script>
 
 <template>
-  <Card class="bg-neutral text-neutral-content group max-md:card-compact" image-aside>
+  <Card
+    :class="[
+      'bg-neutral text-neutral-content group max-md:card-compact',
+      'bg-opacity-70 hover:bg-opacity-100',
+    ]"
+    image-aside
+  >
     <template #image>
       <Icon name="git" class="max-xl:hidden text-9xl opacity-25" />
     </template>
 
     <div class="space-y-2">
-      <div class="flex justify-between items-center">
+      <div class="flex justify-between items-center gap-x-3">
         <Link
           :href="repo.html_url"
           :label="repo.name"
@@ -31,16 +37,17 @@ defineProps({
           hover
         />
 
-        <Divider v-if="repo.homepage" class="max-lg:hidden" horizontal />
-
         <Link
           v-if="repo.homepage"
           :href="repo.homepage"
           target="__blank"
-          variant="primary"
-          label="DEMO"
-          hover
-        />
+          class="opacity-50 group-hover:opacity-100 text-2xl"
+        >
+          <Icon
+            name="play-circle"
+            class="group-hover:animate-pulse hover:group-hover:animate-none"
+          />
+        </Link>
 
         <Badge
           class="max-lg:hidden ml-auto opacity-50"
@@ -50,7 +57,14 @@ defineProps({
 
       <p>{{ repo.description }}</p>
 
-      <div class="flex flex-col opacity-50 group-hover:opacity-100 transition-all gap-2">
+      <div
+        :class="[
+          'flex flex-col gap-2',
+          'grayscale opacity-50',
+          'group-hover:opacity-100 group-hover:grayscale-0',
+          'transition-all duration-300',
+        ]"
+      >
         <LanguageList :languages="repo.languages" />
         <ProgressBar :items="repo.languages" small />
         <TopicList :topics="repo.topics" class="max-sm:hidden" />

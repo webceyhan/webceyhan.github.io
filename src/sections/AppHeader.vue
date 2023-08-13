56b4cf1cc64d1586880f8744a94c8259b98ff3cc
Evren Ceyhan <evren@ceyhan.io>
2023-08-13 12:19:14 +0300

    profile moved into app header

--- src/sections/AppHeader.vue
+++ src/sections/AppHeader.vue
@@ -1,7 +1,10 @@
 <script setup>
+import { ref } from "vue";
 import { useProfile } from "../store/profile";
 import Loader from "../components/Loader.vue";
-import Profile from "../components/Profile.vue";
+import Avatar from "../components/Avatar.vue";
+
+const showLogo = ref(true);
 
 const { profile, loading } = useProfile();
 </script>
@@ -10,11 +13,43 @@ const { profile, loading } = useProfile();
   <header class="hero min-h-screen" :class="$style.cover">
     <!-- <div class="hero-overlay bg-opacity-60"></div> -->
     <div
-      class="hero-content flex-col md:flex-row text-center md:text-left text-neutral-content"
+      class="hero-content text-center md:text-left text-neutral-content"
     >
       <Loader v-if="loading" ring class="w-52" />
 
-      <Profile v-else :profile="profile" />
+      <div v-else class="flex flex-col md:flex-row items-center">
+        <div class="max-w-lg p-5 md:px-20">
+          <Avatar :url="profile.avatar_url" class="max-lg:w-52" />
+        </div>
+
+        <div class="w-full space-y-4">
+          <!-- intro -->
+          <h2 class="text-4xl lg:text-5xl text-neutral-content/60">Hello, I am</h2>
+          <h1 class="text-5xl lg:text-7xl tracking-wide font-semibold">
+            {{ profile.name }}
+          </h1>
+
+          <p class="text-lg lg:text-2xl">
+            <!-- profession -->
+            I'm a <span class="text-warning text-nowrap">{{ profile.bio }}</span>
+
+            <!-- location -->
+            , based in {{ profile.location }}
+
+            <br class="max-sm:hidden" />
+
+            <!-- company -->
+            and working as freelance at
+            <img
+              v-if="showLogo"
+              src="../assets/company-logo.png"
+              class="w-36 h-auto inline-block"
+              @error="showLogo = false"
+            />
+            <span v-else class="text-nowrap">{{ profile.company }}</span>
+          </p>
+        </div>
+      </div>
     </div>
   </header>
 </template>

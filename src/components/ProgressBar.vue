ee16745d4f6d02f47d3c9d1a03c82d31e800dd1a
Evren Ceyhan <evren@ceyhan.io>
2023-08-13 12:09:32 +0300

    progress bar refactored

--- src/components/ProgressBar.vue
+++ src/components/ProgressBar.vue
@@ -1,18 +1,16 @@
 <script setup>
-
 defineProps({
-    items: Array,
+  items: Array,
 });
-
 </script>
 
 <template>
-    <div class="progress" style="height: 5px">
-        <div
-            v-for="({ rate, color }, i) in items"
-            :key="i"
-            class="progress-bar"
-            :style="{ width: `${rate}%`, backgroundColor: color }"
-        ></div>
-    </div>
+  <div class="join w-full h-1.5 bg-base-100 rounded-full">
+    <div
+      v-for="({ rate, color }, i) in items"
+      :key="i"
+      class="join-item h-1.5 rounded-full"
+      :style="{ width: `${rate}%`, backgroundColor: color }"
+    />
+  </div>
 </template>

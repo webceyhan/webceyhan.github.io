ebee1cb97f0b2073a837fbc9ccddf7a15ff2a61e
Evren Ceyhan <evren@ceyhan.io>
2023-08-14 12:39:58 +0300

    repo spacing converted to gap

--- src/sections/partials/RepositoryList.vue
+++ src/sections/partials/RepositoryList.vue
@@ -9,7 +9,7 @@ defineProps({
 <template>
   <transition-group
     tag="div"
-    class="flex flex-col space-y-4"
+    class="w-full flex flex-col gap-y-4"
     enter-active-class="transform-gpu"
     enter-class="opacity-0 translate-x-full"
     enter-to-class="opacity-100 translate-x-0"

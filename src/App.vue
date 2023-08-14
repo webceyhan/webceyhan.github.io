69eb61d1c0a099856d3e456a8e7b2b0ff68820ed
Evren Ceyhan <evren@ceyhan.io>
2023-08-14 19:12:39 +0300

    particles effect added

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,31 @@
 <script setup>
+import { loadSlim } from "tsparticles-slim";
+import { options } from "./particles";
 import AppFooter from "./sections/AppFooter.vue";
 import AppHeader from "./sections/AppHeader.vue";
 import AppProjects from "./sections/AppProjects.vue";
+
+const particlesInit = async (engine) => {
+  //await loadFull(engine);
+  await loadSlim(engine);
+};
+
+const particlesLoaded = async (container) => {
+  console.log("Particles container loaded", container);
+};
 </script>
 
 <template>
   <div class="space-y-10">
     <AppHeader />
+
+    <vue-particles
+      id="tsparticles"
+      :particlesInit="particlesInit"
+      :particlesLoaded="particlesLoaded"
+      :options="options"
+    />
+
     <AppProjects />
     <AppFooter />
   </div>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from 'vue';
+import Particles from 'vue3-particles';
 import App from './App.vue';
 
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import './style.css';
 
-createApp(App).mount('#app');
+createApp(App).use(Particles).mount('#app');

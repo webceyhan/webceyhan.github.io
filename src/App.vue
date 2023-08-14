45b4c93dc144b12299ba983883f651ec06c71071
Evren Ceyhan <evren@ceyhan.io>
2023-08-14 21:40:42 +0300

    particles animation moved to separate file

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,14 @@
 <script setup>
-import { loadSlim } from "tsparticles-slim";
-import { options } from "./particles";
 import AppFooter from "./sections/AppFooter.vue";
 import AppHeader from "./sections/AppHeader.vue";
 import AppProjects from "./sections/AppProjects.vue";
-
-const particlesInit = async (engine) => {
-  //await loadFull(engine);
-  await loadSlim(engine);
-};
-
-const particlesLoaded = async (container) => {
-  console.log("Particles container loaded", container);
-};
+import AppParticles from "./sections/AppParticles.vue";
 </script>
 
 <template>
   <div class="space-y-10">
     <AppHeader />
-
-    <vue-particles
-      id="tsparticles"
-      :particlesInit="particlesInit"
-      :particlesLoaded="particlesLoaded"
-      :options="options"
-    />
-
+    <AppParticles />
     <AppProjects />
     <AppFooter />
   </div>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
 import { createApp } from 'vue';
-import Particles from 'vue3-particles';
 import App from './App.vue';
 
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import './style.css';
 
-createApp(App).use(Particles).mount('#app');
+createApp(App).mount('#app');

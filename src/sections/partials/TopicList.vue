40f8620547f955e1f9bb9d6605ba804f86a1443a
Evren Ceyhan <evren@ceyhan.io>
2023-08-15 15:58:00 +0300

    repo topic list restyled

--- src/sections/partials/TopicList.vue
+++ src/sections/partials/TopicList.vue
@@ -7,8 +7,7 @@ defineProps({
 </script>
 
 <template>
-  <!-- todo: find a clean way to make truncate work while fitting to container -->
-  <div class="h-6 text-clip overflow-hidden space-x-1">
-    <Badge v-for="(topic, i) in topics" :key="i" :label="topic" class="mb-3" />
+  <div class="flex flex-wrap max-h-6 overflow-hidden gap-1">
+    <Badge v-for="(topic, i) in topics" :key="i" :label="topic" />
   </div>
 </template>

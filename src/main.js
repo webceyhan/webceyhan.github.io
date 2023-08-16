bc1380ee7b81bbde05e4e788c94170b68ea94a95
Evren Ceyhan <evren@ceyhan.io>
2023-08-16 13:58:38 +0300

    pinia installed

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 import App from './App.vue';
 
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import './style.css';
 
-createApp(App).mount('#app');
+createApp(App)
+    .use(createPinia())
+    .mount('#app');

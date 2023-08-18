0cfb18a1c293679a9a26b82bfb5baf19d7a51f7d
Evren Ceyhan <evren@ceyhan.io>
2023-08-18 14:25:39 +0300

    social links data splitted

--- server/api/social-links.js
+++ server/api/social-links.js
@@ -1,33 +1,7 @@
-const AVAILABLE_LINKS = [
-    {
-        icon: 'github',
-        url: 'https://github.com',
-        id: process.env.USER_GITHUB,
-    },
-    {
-        icon: 'reddit',
-        url: 'https://www.reddit.com/user',
-        id: process.env.USER_REDDIT,
-    },
-    {
-        icon: 'linkedin',
-        url: 'https://www.linkedin.com/in',
-        id: process.env.USER_LINKEDIN,
-    },
-    {
-        icon: 'twitter',
-        url: 'https://twitter.com',
-        id: process.env.USER_TWITTER,
-    },
-    {
-        icon: 'instagram',
-        url: 'https://www.instagram.com',
-        id: process.env.USER_INSTAGRAM,
-    },
-];
+import LINKS from '../data/social-links';
 
 export default defineEventHandler((event) => {
-    return AVAILABLE_LINKS.filter(havingId).map(normalizeUrl);
+    return LINKS.filter(havingId).map(normalizeUrl);
 });
 
 // HELPERS /////////////////////////////////////////////////////////////////////////////////////////

6a2f5870a6cb968cf3bae27a0888948bf77d1b80
Evren Ceyhan <evren.ceyhan@gmail.com>
2023-12-23 21:36:28 +0100

    disable ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    ssr: false,
     devtools: { enabled: true },
     modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt'],
     css: ['~/assets/css/main.css', 'bootstrap-icons/font/bootstrap-icons.css'],

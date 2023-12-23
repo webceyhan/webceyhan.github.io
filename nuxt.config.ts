6a70b0f9764f3002a15a891dad79e7c0272fdedd
Evren Ceyhan <evren.ceyhan@gmail.com>
2023-12-23 22:38:49 +0100

    restore ssr to true

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    ssr: false,
     devtools: { enabled: true },
     modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt'],
     css: ['~/assets/css/main.css', 'bootstrap-icons/font/bootstrap-icons.css'],
